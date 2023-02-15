e32d5fc4271c12e32e697a26cff187d1aa8851cb
Maxime Maillot <maillot.maxime62@gmail.com>
2023-02-15 11:44:49 +0100

    add prompt to exercice7

--- Javascript/exercices/exercice07/exercice07.js
+++ Javascript/exercices/exercice07/exercice07.js
@@ -19,7 +19,10 @@ function calculateHypothenuse(cote1, cote2) {
 
 let container = document.querySelector(".container");
 
-let cote1 = 2.6, cote2 = 3.9;
+let cote1 = prompt("Donnez la valeur du 1er côté")
+cote1 = parseFloat(cote1)
+let cote2 = prompt("Donnez la valeur du 2ème côté")
+cote2 = parseFloat(cote2)
 appendChildToElement(container, "div", `Les longueurs des côtés adjacent à l'angle droit étant de ${cote1}cm et de ${cote2}cm,`, null, "lead");
 let liste = appendChildToElement(container, "ul")
 appendChildToElement(liste, "li", `la longueur de l'hypothénuse est de ${Math.round(calculateHypothenuse(cote1, cote2) * 100) / 100}cm`)
