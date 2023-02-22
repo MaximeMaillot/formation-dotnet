0b3b073bf320359279101c5cff8bad64cffcdcc9
Maxime Maillot <maillot.maxime62@gmail.com>
2023-02-22 17:02:08 +0100

    add tp1 and start tp2

---
+++ Javascript/TP/TP2/index.js
@@ -0,0 +1,9 @@
+import { appendChildToElement, removeAllChild } from "../../exercices/functions.js";
+
+function main() {
+    document.body.style.backgroundColor = "orange"
+}
+
+window.onload = () => {
+    main()
+};
