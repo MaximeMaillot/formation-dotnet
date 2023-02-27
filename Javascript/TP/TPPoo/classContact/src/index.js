cd177b7acb8bb32f290e8f5d77499a20c2b95e02
Maxime Maillot <maillot.maxime62@gmail.com>
2023-02-27 10:16:03 +0100

    finish tp

--- Javascript/TP/TPPoo/classContact/src/index.js
+++ Javascript/TP/TPPoo/classContact/src/index.js
@@ -8,39 +8,85 @@ const Form = {
     email: document.querySelector("#mail")
 }
 
+const FormAlert = document.querySelector("#form-alert")
+
 const Contacts = document.querySelector("#contacts")
 
 const Ajouter = document.querySelector("#add")
+const Show = document.querySelector("#show")
+
+let timeout;
 
 let contacts = []
 
 function init() {
-    console.log("init")
     Ajouter.addEventListener("click", addContact)
+    Show.addEventListener("click", updateView)
+}
+
+function triggerAlert(msg, time = 3000) {
+    FormAlert.innerText = msg
+    FormAlert.style.display = "block"
+    timeout = setTimeout(() => {
+        FormAlert.style.display = "none"
+    }, time
+    )
+
 }
 
 function updateView() {
+    Form.titre = document.querySelector("input[name=titre]:checked")
+    console.log(Form)
+}
 
+function clearForm() {
+    for (const key in Form) {
+        if (key != "titre") {
+            Form[key].value = ""
+        }
+    }
+
+}
+
+function validateData() {
+    for (const key in Form) {
+        if (Form[key].value == "" || !Form[key].checkValidity()) {
+            console.log(key, Form[key].value, !Form[key].checkValidity())
+            triggerAlert("Formulaire non valide")
+            return false
+        }
+    }
+    return true;
 }
 
 function addContact() {
     Form.titre = document.querySelector("input[name=titre]:checked")
-    const index = contacts.push(new Contact(Form.titre.value, Form.nom.value, Form.prenom.value, Form.dateNaissance.value, Form.telephone.value, Form.email.value)) - 1
+    if (!validateData()) {
+        return false
+    }
+    let contact = new Contact(Form.titre.value, Form.nom.value, Form.prenom.value, Form.dateNaissance.value, Form.telephone.value, Form.email.value)
+    contacts.push(contact)
     const tr = document.createElement("tr")
+    tr.setAttribute("data-id", contact.id)
     Contacts.appendChild(tr)
-    for (const key in contacts[index]) {
+    for (const key in contact) {
         const td = document.createElement("td")
-        td.innerText = contacts[index][key]
+        td.innerText = contact[key]
         tr.appendChild(td)
     }
     const td = document.createElement("td")
-    tr.appendChild(td)
     const deleteDiv = document.createElement("i")
     deleteDiv.classList.add(["fa-solid"], ["fa-trash"])
     td.appendChild(deleteDiv)
+    tr.appendChild(td)
     deleteDiv.addEventListener("click", () => {
+        let index = parseInt(tr.getAttribute("data-id"))
+        contacts = contacts.filter((c) => {
+            return c.id != Number(index)
+        })
         tr.remove()
     })
+    clearForm()
 }
 
 init()
